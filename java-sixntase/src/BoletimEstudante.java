bdf487a2ad9a93a76c2dd3c9403229f26383be4e
Filipe062 <luisfhylipe@gmail.com>
2025-05-16 18:25:16 -0300

    Aula-1

---
+++ java-sixntase/src/BoletimEstudante.java
@@ -0,0 +1,14 @@
+public class BoletimEstudante {
+   
+    public static void main(String[] args) {
+        int mediaFinal = 7;
+        if (mediaFinal<6)
+             System.out.println("REPROVADOO");
+        else if(mediaFinal==6)
+             System.out.println("PROVA MINERVA");
+        else
+             System.out.println("APROVADO"); 
+             
+    } 
+    
+}
