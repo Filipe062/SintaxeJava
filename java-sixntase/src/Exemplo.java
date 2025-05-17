c5f2d2b5e8b541ea584d8a527a1528fb094af23a
Filipe062 <luisfhylipe@gmail.com>
2025-05-17 11:02:26 -0300

    Organizaçao

---
+++ java-sixntase/src/Exemplo.java
@@ -0,0 +1,14 @@
+public class Exemplo {
+   
+   public Exemplo() {
+   }
+
+   public static void main(String[] var0) {
+      boolean var1 = true;
+      byte var2 = 10;
+      System.out.println(var2);
+      System.out.println(3.14);
+   }
+}
+
+
